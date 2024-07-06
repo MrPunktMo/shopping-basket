8e1fae06778ab112c954a708380c97fc5cb00acb
mrpunktmo <mrpunktmo@gmail.com>
2024-07-06 11:48:16 +0200

    Initial commit

---
+++ src/test/java/org/triplem/shoppingbasket/ShoppingBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.triplem.shoppingbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBasketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
